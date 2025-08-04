846a83b8b50147ff5e2f2af481ac38d51441a3aa
Juan Pablo <juanpabloaquino45@gmail.com>
2025-08-04 11:36:49 -0300

    Update ItemVenda.java

--- src/ItemVenda.java
+++ src/ItemVenda.java
@@ -1,1 +1,47 @@
 
+package itemvenda;
+
+
+public class ItemVenda {
+   private Produto produto;
+   private int quant;
+
+    public ItemVenda(Produto produto, int quant) {
+        this.produto = produto;
+        this.quant = quant;
+        
+    }
+
+  
+    public Produto getProduto() {
+        return produto;
+    }
+
+    
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+  
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    
+    }
+    public float calcularTotalItem(float total){
+            
+        total = produto.getIdProduto(produto.getPrecoUnitario())* quant;
+        return total;
+            
+        }
+    public float calcularTotalItemComDesco(float totaldesc){
+        totaldesc = 
+    }
+
+
+  
+    
+}
