4a18a473cdf6ecea3dce7442b0297277a8d23fd1
WendelNeres <wendelneres.dev@gmail.com>
2025-08-04 10:59:34 -0300

    criação das classes produto e eletrônico

---
+++ src/Eletronico.java
@@ -0,0 +1,26 @@
+public class Eletronico extends Produto{
+    private int garantiaMeses;
+    private String voltagem;
+
+    public Eletronico(int idProduto, String nome, float precoUnitario, int quantEstoque, int garantiaMeses, String voltagem) {
+        super(idProduto, nome, precoUnitario, quantEstoque);
+        this.garantiaMeses = garantiaMeses;
+        this.voltagem = voltagem;
+    }
+
+    public int getGarantiaMeses() {
+        return garantiaMeses;
+    }
+
+    public void setGarantiaMeses(int garantiaMeses) {
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    public String getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(String voltagem) {
+        this.voltagem = voltagem;
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,45 @@
+public class Produto {
+    protected int idProduto;
+    protected String nome;
+    protected float precoUnitario;
+    protected int quantEstoque;
+
+    public Produto(int idProduto, String nome, float precoUnitario, int quantEstoque) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.quantEstoque = quantEstoque;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(float precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantEstoque() {
+        return quantEstoque;
+    }
+
+    public void setQuantEstoque(int quantEstoque) {
+        this.quantEstoque = quantEstoque;
+    }
+}
