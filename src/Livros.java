021be684713a88ec3de55e7579a44b10675587c3
Cristyan Alves. <alvescristyan606@gmail.com>
2025-08-04 11:23:01 -0300

    C) Classe LIvros

---
+++ src/Livros.java
@@ -0,0 +1,38 @@
+public class Livros extends Produto {
+    String autor;
+    int numeroPaginas;
+
+    public Livros(String autor,int numeroPaginas){
+       
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+        
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public double precoComDesconto() {
+        return (float) (this.PrecoUnitario * 0.85);
+    }
+
+    public String toString() {
+        return toString() +
+               "\nAutor: " + autor +
+               "\nNúmero de Páginas: " + numeroPaginas +
+               "\nPreço com Desconto: " + precoComDesconto();
+    }
+}
